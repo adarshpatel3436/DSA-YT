d7bbe921f558ac7a870779081940234e06b913fa
adarshpatel3436 <pateladarsh3436@gmail.com>
2025-12-30 21:04:47 +0530

    new

---
+++ src/basicDSA/inverseANumber.java
@@ -0,0 +1,27 @@
+package basicDSA;
+
+import java.util.Scanner;
+
+public class inverseANumber {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:  ");
+        int n = sc.nextInt();
+
+        int inv = 0;
+        int op = 1;       // original place
+
+        while(n != 0){
+            int od = n % 10;   // original digit
+            int id = op;
+            int ip = od;
+
+            inv = inv + id * (int)Math.pow(10 , ip - 1);
+
+            n = n/10;
+            op++;
+
+        }
+        System.out.println(inv);
+    }
+}
---
+++ src/basicDSA/numberReverse.java
@@ -0,0 +1,17 @@
+package basicDSA;
+
+import java.util.Scanner;
+
+public class numberReverse {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        while(n != 0){
+            int dig = n % 10 ;
+            n = n/10;
+            System.out.println(dig);
+        }
+    }
+}
