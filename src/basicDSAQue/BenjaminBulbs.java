053140c36ae30bb0c0c37447a48f904558a8cfb8
adarshpatel3436 <pateladarsh3436@gmail.com>
2025-12-31 13:42:59 +0530

    Benjamin Bulbs

--- src/basicDSAQue/BenjaminBulbs.java
+++ src/basicDSAQue/BenjaminBulbs.java
@@ -1,5 +1,17 @@
 package basicDSAQue;
 
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
 public class BenjaminBulbs {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i * i  <= n  ; i++) {
+            System.out.println(i * i);
+        }
+    }
 
 }
