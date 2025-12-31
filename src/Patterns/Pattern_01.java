6592660402576bb5474bfd64980eeaee69361682
adarshpatel3436 <pateladarsh3436@gmail.com>
2025-12-31 14:08:38 +0530

    Pattern-1

---
+++ src/Patterns/Pattern_01.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_01 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* \t");
+            }
+            System.out.println();
+        }
+    }
+}
