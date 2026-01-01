ceda80f0c6094f0c076445751ac000e5b73b4710
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-01-02 01:07:18 +0530

    pattern-8

---
+++ src/Patterns/pattern_08.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class pattern_08 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (i + j == n + 1) {
+                    System.out.print("* \t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
