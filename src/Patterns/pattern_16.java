77178aa7e5b0c93228da6b3fafc8880ada1031ea
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-01-04 23:47:52 +0530

    pattern-16

---
+++ src/Patterns/pattern_16.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class pattern_16 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int sp = 2* n-3;
+        int st = 1;
+        int val = 1;
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= st; j++) {
+                System.out.print(val +"\t");
+                val++;
+            }
+
+            for (int j = 1; j <= sp ; j++) {
+                System.out.print("\t");
+            }
+            if(i == n){
+                st--;
+                val--;
+            }
+            for (int j = 1; j <= st ; j++) {
+                val--;
+                System.out.print(val +"\t");
+
+            }
+
+            st++;
+            sp -= 2;
+            System.out.println();
+        }
+    }
+}
