96c3cd499dfc99dfd8604158573fcdf2bd9c3d3b
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-01-17 23:03:51 +0530

    bar chart

---
+++ src/ArrayLvl1/barChart.java
@@ -0,0 +1,33 @@
+package ArrayLvl1;
+
+import java.util.Scanner;
+
+public class barChart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 0; i< arr.length ; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        for(int floar = max ; floar >= 1; floar--){
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] >= floar){
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
