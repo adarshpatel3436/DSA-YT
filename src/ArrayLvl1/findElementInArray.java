5a14c38672b249b7205d4d1c91c6b8d077ebff42
adarshpatel3436 <pateladarsh3436@gmail.com>
2026-01-17 10:32:12 +0530

    find element in array

---
+++ src/ArrayLvl1/findElementInArray.java
@@ -0,0 +1,24 @@
+package ArrayLvl1;
+
+import java.util.Scanner;
+
+public class findElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array:  ");
+        int n1 = sc.nextInt();
+        int[] arr = new int[n1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int data = sc.nextInt();
+        int idx = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == data){
+                idx = i;
+                break;
+            }
+        }
+        System.out.println(idx);
+    }
+}
